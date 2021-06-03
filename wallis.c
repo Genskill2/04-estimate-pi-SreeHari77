63c85954a6ddca85d28e47719047be6b3073e4ee
SreeHari77 <84658632+SreeHari77@users.noreply.github.com>
2021-06-03 18:31:30 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,3 +23,10 @@ int main(void) {
     }
   }
 }
+float wallis_pi(int x){
+  float prod=1;
+  for(int n;n<=x;n++){
+    prod*=(float)4*n*n/(4*n*n-1);}
+   float result=2*prod;
+    return result;
+}
