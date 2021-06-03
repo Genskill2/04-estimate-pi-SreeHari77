d36220bb130d35cb33361a2342cf2a49d991903f
SreeHari77 <84658632+SreeHari77@users.noreply.github.com>
2021-06-03 22:33:47 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -25,9 +25,9 @@ int main(void) {
 }
 
 float wallis_pi(int n){
-  float prod=1.0;
-  for(int i=1; i<=n; i++){
-    prod*= (float)4.0*i*i/(4.0*i*i)-1.0 ;
+  float prod=1.0000;
+  for(int i = 1; i <= n; i++){
+    prod = prod * (4.0 * i * i) / (4.0 * i * i - 1) ;
    }
   float result= 2.0*prod;
   
